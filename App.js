78f6026bfc55da7d02e8f610dd978e3ec615daf1
projectmanager1221 <projectmanager1221@gmail.com>
2017-11-15 04:15:38 +0530

    implement reusable component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './src/component/common';
 
 class App extends Component {
     render() {
         return (
             <View>
+                <Header headerText='Authentication' />
                 <Text>An App </Text>
                 </View>
         )
